d155add8b9c114da3e3dfec71e42b8b554c74229
Anton Shelkov <shelkovaf@mos.ru>
2023-09-18 09:32:27 +0300

    change naming

--- internal/handlers/handler.go
+++ internal/handlers/handler.go
@@ -29,23 +29,23 @@ func RegisterRoutes(parent *echo.Group, h Handler) {
 }
 
 func (h *Handler) login(c echo.Context) error {
-	userApi := &models.User{}
+	userAPI := &models.User{}
 
-	if err := c.Bind(userApi); err != nil {
+	if err := c.Bind(userAPI); err != nil {
 		return c.String(http.StatusBadRequest, "Not valid request")
 	}
 
-	if err := c.Validate(userApi); err != nil {
+	if err := c.Validate(userAPI); err != nil {
 		return c.String(http.StatusBadRequest, "Validation error")
 	}
 
-	userDB, err := h.Store.GetUserByLogin(userApi.Login)
+	userDB, err := h.Store.GetUserByLogin(userAPI.Login)
 
 	if err != nil && err != storage.ErrNoDBResult {
 		return c.String(http.StatusInternalServerError, "Unexpected error")
 	}
 
-	if !utils.CheckHashAndPassword(userDB.Password, userApi.Password) {
+	if !utils.CheckHashAndPassword(userDB.Password, userAPI.Password) {
 		return c.String(http.StatusUnauthorized, "Unauthorized user")
 	}
 
@@ -58,27 +58,27 @@ func (h *Handler) login(c echo.Context) error {
 }
 
 func (h *Handler) register(c echo.Context) error {
-	userApi := &models.User{}
+	userAPI := &models.User{}
 
-	if err := c.Bind(userApi); err != nil {
+	if err := c.Bind(userAPI); err != nil {
 		return c.String(http.StatusBadRequest, "Not valid request")
 	}
 
-	if err := c.Validate(userApi); err != nil {
+	if err := c.Validate(userAPI); err != nil {
 		return c.String(http.StatusBadRequest, "Validation error")
 	}
 
-	userDB, err := h.Store.GetUserByLogin(userApi.Login)
+	userDB, err := h.Store.GetUserByLogin(userAPI.Login)
 	switch {
 	case err == nil && userDB.Login != "":
 		return c.String(http.StatusConflict, "Login exists")
 	case err == storage.ErrNoDBResult:
-		cryptedPsw, err := utils.HashString(userApi.Password)
+		cryptedPsw, err := utils.HashString(userAPI.Password)
 		if err != nil {
 			return c.String(http.StatusInternalServerError, "Internal server error")
 		}
 
-		userID, err := h.Store.Register(userApi.Login, cryptedPsw)
+		userID, err := h.Store.Register(userAPI.Login, cryptedPsw)
 		if err != nil {
 			return c.String(http.StatusInternalServerError, "Internal server error")
 		}
